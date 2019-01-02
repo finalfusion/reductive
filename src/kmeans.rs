4c1e01102e001b354d60f07143414730e8290031
Daniël de Kok <me@danieldk.eu>
2019-01-02 09:19:20 +0100

    Add k-means helper methods
    - cluster_assignments: find for every instance the nearest cluster
      centroid.
    - update_centroids: update cluster centroids based on the instances
      assigned to that cluster.

--- src/kmeans.rs
+++ src/kmeans.rs
@@ -1,10 +1,14 @@
 //! K-means clustering.
 
 use std::collections::HashSet;
+use std::iter::Sum;
 
-use ndarray::{Array2, ArrayBase, Axis, Data, Ix2, NdFloat};
+use ndarray::{Array1, Array2, ArrayBase, ArrayView2, ArrayViewMut2, Axis, Data, Ix2, NdFloat};
+use ordered_float::OrderedFloat;
 use rand::distributions::{Distribution, Uniform};
 use rand::Rng;
+
+use crate::linalg::SquaredEuclideanDistance;
 
 /// Initial centroid selection.
 pub trait InitialCentroids<A> {
@@ -81,5 +85,143 @@ pub struct NIterationsCondition(pub usize);
 impl<A> StopCondition<A> for NIterationsCondition {
     fn should_stop(&mut self, iteration: usize, _loss: A) -> bool {
         iteration >= self.0
+    }
+}
+
+/// Find nearest cluster centroid for each instance.
+///
+/// Find nearest centroid for each instance along `instance_axis` of
+/// `instances`. Returns for each instance the index of the nearest
+/// cluster centroid.
+fn cluster_assignments<A>(
+    centroids: ArrayView2<A>,
+    instances: ArrayView2<A>,
+    instance_axis: Axis,
+) -> Vec<usize>
+where
+    A: NdFloat + Sum,
+{
+    let mut assignments = Vec::with_capacity(instances.len_of(instance_axis));
+
+    let dists = if instance_axis == Axis(0) {
+        instances.squared_euclidean_distance(centroids)
+    } else {
+        instances.t().squared_euclidean_distance(centroids)
+    };
+
+    for inst_dists in dists.outer_iter() {
+        assignments.push(
+            inst_dists
+                .iter()
+                .enumerate()
+                .min_by_key(|v| OrderedFloat(*v.1))
+                .unwrap()
+                .0,
+        );
+    }
+
+    assignments
+}
+
+/// Update centroids to the mean of the assigned data points.
+///
+/// `instance_axis` is the instance axis of `data`. The centroids
+/// are row-based. `assignments` contains an assignment for each
+/// data point.
+fn update_centroids<A>(
+    mut centroids: ArrayViewMut2<A>,
+    data: ArrayView2<A>,
+    instance_axis: Axis,
+    assignments: &[usize],
+) where
+    A: NdFloat,
+{
+    assert_eq!(
+        assignments.len(),
+        data.len_of(instance_axis),
+        "The number of assignments should be equal to the number of instances."
+    );
+
+    centroids.fill(A::zero());
+
+    let mut centroid_counts = Array1::zeros(centroids.rows());
+
+    for (instance, assignment) in data.axis_iter(instance_axis).zip(assignments) {
+        let mut centroid = centroids.index_axis_mut(Axis(0), *assignment);
+        centroid += &instance;
+        centroid_counts[*assignment] += A::one();
+    }
+
+    for (mut centroid, centroid_count) in centroids
+        .axis_iter_mut(instance_axis)
+        .zip(centroid_counts.outer_iter())
+    {
+        if centroid_count[()] > A::zero() {
+            centroid /= &centroid_count;
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use ndarray::{array, Axis};
+
+    use super::{cluster_assignments, update_centroids};
+
+    #[test]
+    fn correct_cluster_assignments() {
+        let centroids = array![[0.5, 0., 0.], [0., -1., 0.], [0., 0., 1.], [0., 1., 1.]];
+        let instances = array![
+            [0., 0.5, 0.],
+            [0., 0., 2.],
+            [1., 0., 0.],
+            [0., 0., 1.],
+            [0., -2., 0.],
+            [0., 0.7, 0.7],
+            [0., 0., 0.]
+        ];
+
+        // Test instances along axis 0.
+        let assignments = cluster_assignments(centroids.view(), instances.view(), Axis(0));
+        assert_eq!(assignments, &[0, 2, 0, 2, 1, 3, 0]);
+
+        // Test instances along axis 1.
+        let assignments = cluster_assignments(centroids.view(), instances.t(), Axis(1));
+        assert_eq!(assignments, &[0, 2, 0, 2, 1, 3, 0]);
+    }
+
+    #[test]
+    fn correct_update_centroids() {
+        let mut centroids = array![[1., 0., 0.], [0., 1., 0.], [0., 0., 1.]];
+        let instances = array![
+            [-1., -1., 0.],
+            [1., 1., 0.],
+            [-2., -1., 0.],
+            [0., 0., 0.],
+            [0., 0., 1.],
+            [0., 0., 2.],
+        ];
+        let assignments = vec![1, 0, 1, 0, 2, 2];
+
+        // Test instances along axis 0.
+        update_centroids(
+            centroids.view_mut(),
+            instances.view(),
+            Axis(0),
+            &assignments,
+        );
+
+        assert_eq!(
+            centroids,
+            array![[0.5, 0.5, 0.], [-1.5, -1., 0.], [0., 0., 1.5]]
+        );
+
+        // Test instances along axis 1.
+        update_centroids(centroids.view_mut(), instances.t(), Axis(1), &assignments);
+
+        assert_eq!(
+            centroids,
+            array![[0.5, 0.5, 0.], [-1.5, -1., 0.], [0., 0., 1.5]]
+        );
     }
 }
