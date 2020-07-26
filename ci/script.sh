7ebc75aae631c292040c9538f40c7b33cf88bfbe
Daniël de Kok <me@danieldk.eu>
2020-07-26 17:20:12 +0200

    Do not provide features for BLAS implementations
    The implementation should be selected by the final program that uses it.

--- ci/script.sh
+++ ci/script.sh
@@ -3,12 +3,12 @@
 set -euxo pipefail
 
 # On Rust 1.32.0, we only care about passing tests.
-if rustc --version | grep -v "^rustc 1.32.0"; then
+if rustc --version | grep -v "^rustc 1.37.0"; then
   cargo fmt --all -- --check
   cargo clippy -- -D warnings
 fi
 
 cargo build --verbose
 cargo test --verbose
-cargo build --verbose --features "openblas"
-cargo test --verbose --features "openblas"
+cargo build --verbose --features "openblas-test"
+cargo test --verbose --features "openblas-test"
