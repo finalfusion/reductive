32aec67fdbf1243f9642baa2d683bae0197508a3
Daniël de Kok <me@danieldk.eu>
2021-02-21 14:01:51 +0100

    CI: run OPQ train tests in regular GH Actions workflow

--- ci.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs ? import (import ./nix/sources.nix).nixpkgs {} }:
-
-let
-  finalfusion = pkgs.callPackage ./default.nix {};
-in [
-  finalfusion
-  (finalfusion.override { withOpenblas = true; })
-]
