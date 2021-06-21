f77175dd3e61d4f70eafadb20225dcc0e3aa103c
Daniël de Kok <me@danieldk.eu>
2021-06-21 15:21:42 +0200

    Use fixed seeds when generating random matrices in tests

--- src/pq/pq.rs
+++ src/pq/pq.rs
@@ -348,6 +348,8 @@ where
 mod tests {
     use ndarray::{array, Array1, Array2, Array3, ArrayView2};
     use rand::distributions::Uniform;
+    use rand::SeedableRng;
+    use rand_xorshift::XorShiftRng;
 
     use super::PQ;
     use crate::linalg::EuclideanDistance;
@@ -425,9 +427,10 @@ mod tests {
 
     #[test]
     fn quantize_with_pq() {
+        let mut rng = XorShiftRng::seed_from_u64(42);
         let uniform = Uniform::new(0f32, 1f32);
-        let instances = Array2::random((256, 20), uniform);
-        let pq = PQ::train_pq(10, 7, 10, 1, instances.view());
+        let instances = Array2::random_using((256, 20), uniform, &mut rng);
+        let pq = PQ::train_pq_using(10, 7, 10, 1, instances.view(), rng);
         let loss = avg_euclidean_loss(instances.view(), &pq);
         // Loss is around 0.077.
         assert!(loss < 0.08);
