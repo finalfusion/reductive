6b4dd70e395b8b554a1e8bbac634012717c5e9c9
Daniël de Kok <me@danieldk.eu>
2021-06-23 16:13:47 +0200

    Fix benchmarks

--- benches/linalg.rs
+++ benches/linalg.rs
@@ -3,15 +3,15 @@
 extern crate test;
 
 use ndarray::{Array2, Axis};
-use ndarray_rand::RandomExt;
-use rand::distributions::Normal;
+use rand_distr::Normal;
 use test::Bencher;
 
 use reductive::linalg::{Covariance, SquaredEuclideanDistance};
+use reductive::ndarray_rand::RandomExt;
 
 #[bench]
 fn covariance_axis0(bencher: &mut Bencher) {
-    let data: Array2<f64> = Array2::random((50, 100), Normal::new(1., 0.2));
+    let data: Array2<f64> = Array2::random((50, 100), Normal::new(1., 0.2).unwrap());
 
     bencher.iter(|| {
         data.view().covariance(Axis(0));
@@ -20,7 +20,7 @@ fn covariance_axis0(bencher: &mut Bencher) {
 
 #[bench]
 fn covariance_axis1(bencher: &mut Bencher) {
-    let data: Array2<f64> = Array2::random((100, 50), Normal::new(1., 0.2));
+    let data: Array2<f64> = Array2::random((100, 50), Normal::new(1., 0.2).unwrap());
 
     bencher.iter(|| {
         data.view().covariance(Axis(1));
@@ -29,8 +29,8 @@ fn covariance_axis1(bencher: &mut Bencher) {
 
 #[bench]
 fn squared_euclidean_distance_ix1_ix1(bencher: &mut Bencher) {
-    let data1: Array2<f64> = Array2::random((200, 50), Normal::new(1., 0.2));
-    let data2: Array2<f64> = Array2::random((50, 50), Normal::new(1., 0.2));
+    let data1: Array2<f64> = Array2::random((200, 50), Normal::new(1., 0.2).unwrap());
+    let data2: Array2<f64> = Array2::random((50, 50), Normal::new(1., 0.2).unwrap());
 
     bencher.iter(|| {
         for row1 in data1.outer_iter() {
@@ -43,8 +43,8 @@ fn squared_euclidean_distance_ix1_ix1(bencher: &mut Bencher) {
 
 #[bench]
 fn squared_euclidean_distance_ix1_ix2(bencher: &mut Bencher) {
-    let data1: Array2<f64> = Array2::random((200, 50), Normal::new(1., 0.2));
-    let data2: Array2<f64> = Array2::random((50, 50), Normal::new(1., 0.2));
+    let data1: Array2<f64> = Array2::random((200, 50), Normal::new(1., 0.2).unwrap());
+    let data2: Array2<f64> = Array2::random((50, 50), Normal::new(1., 0.2).unwrap());
 
     bencher.iter(|| {
         for row in data1.outer_iter() {
@@ -55,8 +55,8 @@ fn squared_euclidean_distance_ix1_ix2(bencher: &mut Bencher) {
 
 #[bench]
 fn squared_euclidean_distance_ix2_ix2(bencher: &mut Bencher) {
-    let data1: Array2<f64> = Array2::random((200, 50), Normal::new(1., 0.2));
-    let data2: Array2<f64> = Array2::random((50, 50), Normal::new(1., 0.2));
+    let data1: Array2<f64> = Array2::random((200, 50), Normal::new(1., 0.2).unwrap());
+    let data2: Array2<f64> = Array2::random((50, 50), Normal::new(1., 0.2).unwrap());
 
     bencher.iter(|| {
         data1.view().squared_euclidean_distance(data2.view());
--- benches/pq.rs
+++ benches/pq.rs
@@ -2,31 +2,31 @@
 
 extern crate test;
 
-use ndarray::Array2;
-use ndarray_rand::RandomExt;
-use rand::distributions::Normal;
+use ndarray::{Array1, Array2};
+use rand_distr::Normal;
 use test::Bencher;
 
+use reductive::ndarray_rand::RandomExt;
 use reductive::pq::{QuantizeVector, TrainPQ, PQ};
 
 #[bench]
 fn pq_quantize(bencher: &mut Bencher) {
-    let data: Array2<f64> = Array2::random((100, 128), Normal::new(0., 1.));
-    let pq = PQ::train_pq(16, 4, 10, 1, data.view());
+    let data: Array2<f64> = Array2::random((100, 128), Normal::new(0., 1.).unwrap());
+    let pq = PQ::train_pq(16, 4, 10, 1, data.view()).unwrap();
 
     bencher.iter(|| {
         for v in data.outer_iter() {
-            pq.quantize_vector(v);
+            let _: Array1<u8> = pq.quantize_vector(v);
         }
     })
 }
 
 #[bench]
 fn pq_quantize_batch(bencher: &mut Bencher) {
-    let data: Array2<f64> = Array2::random((100, 128), Normal::new(0., 1.));
-    let pq = PQ::train_pq(16, 4, 10, 1, data.view());
+    let data: Array2<f64> = Array2::random((100, 128), Normal::new(0., 1.).unwrap());
+    let pq = PQ::train_pq(16, 4, 10, 1, data.view()).unwrap();
 
     bencher.iter(|| {
-        pq.quantize_batch(data.view());
+        let _: Array2<u8> = pq.quantize_batch(data.view());
     })
 }
