3f9293187313de5193af09e193432173e9874b0c
Daniël de Kok <me@danieldk.eu>
2021-11-29 09:47:38 +0100

    Convert benchmarks to use criterion

--- benches/pq.rs
+++ benches/pq.rs
@@ -1,32 +1,31 @@
-#![feature(test)]
-
-extern crate test;
-
+use criterion::{criterion_group, criterion_main, Criterion};
 use ndarray::{Array1, Array2};
 use rand_distr::Normal;
-use test::Bencher;
 
 use reductive::ndarray_rand::RandomExt;
 use reductive::pq::{QuantizeVector, TrainPq, Pq};
 
-#[bench]
-fn pq_quantize(bencher: &mut Bencher) {
+fn pq_quantize(c: &mut Criterion) {
     let data: Array2<f64> = Array2::random((100, 128), Normal::new(0., 1.).unwrap());
     let pq = Pq::train_pq(16, 4, 10, 1, data.view()).unwrap();
 
-    bencher.iter(|| {
-        for v in data.outer_iter() {
-            let _: Array1<u8> = pq.quantize_vector(v);
-        }
-    })
+    c.bench_function("pq_quantize", |b| {
+        b.iter(|| {
+            for v in data.outer_iter() {
+                let _: Array1<u8> = pq.quantize_vector(v);
+            }
+        })
+    });
 }
 
-#[bench]
-fn pq_quantize_batch(bencher: &mut Bencher) {
+fn pq_quantize_batch(c: &mut Criterion) {
     let data: Array2<f64> = Array2::random((100, 128), Normal::new(0., 1.).unwrap());
     let pq = Pq::train_pq(16, 4, 10, 1, data.view()).unwrap();
 
-    bencher.iter(|| {
-        let _: Array2<u8> = pq.quantize_batch(data.view());
-    })
+    c.bench_function("pq_quantize_batch", |b| {
+        b.iter(|| pq.quantize_batch::<u8, _>(data.view()))
+    });
 }
+
+criterion_group!(benches, pq_quantize, pq_quantize_batch);
+criterion_main!(benches);
