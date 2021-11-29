4b5a9b568bb8cb5827c95dd0fdd29a27ca1ce91a
Daniël de Kok <me@danieldk.eu>
2021-11-29 10:47:14 +0100

    Add benchmarks for reconstruct/reconstruct_batch

--- benches/pq.rs
+++ benches/pq.rs
@@ -3,7 +3,7 @@ use ndarray::{Array1, Array2};
 use rand_distr::Normal;
 
 use reductive::ndarray_rand::RandomExt;
-use reductive::pq::{Pq, QuantizeVector, TrainPq};
+use reductive::pq::{Pq, QuantizeVector, ReconstructVector, TrainPq};
 
 pub fn create_quantizer() -> (Pq<f32>, Array2<f32>) {
     let data: Array2<f32> = Array2::random((100, 128), Normal::new(0., 1.).unwrap());
@@ -32,5 +32,33 @@ fn pq_quantize_batch(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, pq_quantize, pq_quantize_batch);
+fn pq_reconstruct(c: &mut Criterion) {
+    let (pq, data) = create_quantizer();
+    let quantized = pq.quantize_batch::<u8, _>(data);
+
+    c.bench_function("pq_reconstruct", |b| {
+        b.iter(|| {
+            for q in quantized.outer_iter() {
+                pq.reconstruct_vector(q);
+            }
+        })
+    });
+}
+
+fn pq_reconstruct_batch(c: &mut Criterion) {
+    let (pq, data) = create_quantizer();
+    let quantized = pq.quantize_batch::<u8, _>(data);
+
+    c.bench_function("pq_reconstruct_batch", |b| {
+        b.iter(|| pq.reconstruct_batch(quantized.view()))
+    });
+}
+
+criterion_group!(
+    benches,
+    pq_quantize,
+    pq_quantize_batch,
+    pq_reconstruct,
+    pq_reconstruct_batch
+);
 criterion_main!(benches);
