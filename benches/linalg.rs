d3c9dc1597b504cd9538a2eb8226f052e785f8f0
Daniël de Kok <me@danieldk.eu>
2021-11-29 09:47:38 +0100

    Convert benchmarks to use criterion

--- benches/linalg.rs
+++ benches/linalg.rs
@@ -1,64 +1,69 @@
-#![feature(test)]
-
-extern crate test;
-
+use criterion::{criterion_group, criterion_main, Criterion};
 use ndarray::{Array2, Axis};
 use rand_distr::Normal;
-use test::Bencher;
 
 use reductive::linalg::{Covariance, SquaredEuclideanDistance};
 use reductive::ndarray_rand::RandomExt;
 
-#[bench]
-fn covariance_axis0(bencher: &mut Bencher) {
+fn covariance_axis0(c: &mut Criterion) {
     let data: Array2<f64> = Array2::random((50, 100), Normal::new(1., 0.2).unwrap());
 
-    bencher.iter(|| {
-        data.view().covariance(Axis(0));
-    })
+    c.bench_function("covariance_axis0", |b| {
+        b.iter(|| data.view().covariance(Axis(0)))
+    });
 }
 
-#[bench]
-fn covariance_axis1(bencher: &mut Bencher) {
+fn covariance_axis1(c: &mut Criterion) {
     let data: Array2<f64> = Array2::random((100, 50), Normal::new(1., 0.2).unwrap());
 
-    bencher.iter(|| {
-        data.view().covariance(Axis(1));
-    })
+    c.bench_function("covariance_axis1", |b| {
+        b.iter(|| data.view().covariance(Axis(1)))
+    });
 }
 
-#[bench]
-fn squared_euclidean_distance_ix1_ix1(bencher: &mut Bencher) {
+fn squared_euclidean_distance_ix1_ix1(c: &mut Criterion) {
     let data1: Array2<f64> = Array2::random((200, 50), Normal::new(1., 0.2).unwrap());
     let data2: Array2<f64> = Array2::random((50, 50), Normal::new(1., 0.2).unwrap());
 
-    bencher.iter(|| {
-        for row1 in data1.outer_iter() {
-            for row2 in data2.outer_iter() {
-                row1.squared_euclidean_distance(row2);
+    c.bench_function("squared_euclidean_distance_ix1_ix1", |b| {
+        b.iter(|| {
+            for row1 in data1.outer_iter() {
+                for row2 in data2.outer_iter() {
+                    row1.squared_euclidean_distance(row2);
+                }
             }
-        }
-    })
+        })
+    });
 }
 
-#[bench]
-fn squared_euclidean_distance_ix1_ix2(bencher: &mut Bencher) {
+fn squared_euclidean_distance_ix1_ix2(c: &mut Criterion) {
     let data1: Array2<f64> = Array2::random((200, 50), Normal::new(1., 0.2).unwrap());
     let data2: Array2<f64> = Array2::random((50, 50), Normal::new(1., 0.2).unwrap());
 
-    bencher.iter(|| {
-        for row in data1.outer_iter() {
-            row.squared_euclidean_distance(data2.view());
-        }
-    })
+    c.bench_function("squared_euclidean_distance_ix1_ix2", |b| {
+        b.iter(|| {
+            for row in data1.outer_iter() {
+                row.squared_euclidean_distance(data2.view());
+            }
+        })
+    });
 }
 
-#[bench]
-fn squared_euclidean_distance_ix2_ix2(bencher: &mut Bencher) {
+fn squared_euclidean_distance_ix2_ix2(c: &mut Criterion) {
     let data1: Array2<f64> = Array2::random((200, 50), Normal::new(1., 0.2).unwrap());
     let data2: Array2<f64> = Array2::random((50, 50), Normal::new(1., 0.2).unwrap());
 
-    bencher.iter(|| {
-        data1.view().squared_euclidean_distance(data2.view());
-    })
+    c.bench_function("squared_euclidean_distance_ix2_ix2", |b| {
+        b.iter(|| data1.view().squared_euclidean_distance(data2.view()))
+    });
 }
+
+criterion_group!(
+    benches,
+    covariance_axis0,
+    covariance_axis1,
+    squared_euclidean_distance_ix1_ix2,
+    squared_euclidean_distance_ix2_ix2,
+    squared_euclidean_distance_ix1_ix1
+);
+criterion_main!(benches);
